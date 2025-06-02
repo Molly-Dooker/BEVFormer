e55668388c2e5f59e0b6fc413b6989e5c0d1c82d
lsm0729 <lsm0729@bos-semi.com>
2025-06-02 17:17:15 +0900

    ..

---
+++ RUN3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 5시간 대기 (5시간 * 60분 * 60초)
+sleep 1800
+
+
+# 9부터 23까지 반복 실행
+for NUMBER in {9..23}
+do
+  echo "NUMBER ${NUMBER}에 대한 테스트를 시작합니다."
+  ./tools/dist_test.sh ./projects/configs/bevformer/bevformer_base.py ./ckpts/bevformer_r101_dcn_24ep.pth 8 --prefix test_${NUMBER}
+  echo "NUMBER ${NUMBER}에 대한 테스트를 완료했습니다."
+done
+
+echo "모든 테스트가 완료되었습니다."
