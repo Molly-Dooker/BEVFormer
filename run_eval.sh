431a67f138733ae80e74072699bdef2027fa1d0e
lsm0729 <lsm0729@bos-semi.com>
2025-06-09 09:38:42 +0900

    ..

--- run_eval.sh
+++ run_eval.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # 5시간 대기 (5시간 * 60분 * 60초)
-# sleep 1800
+echo "대기시작"
+sleep 18000
 
 
 
-for NUMBER in {24..32}
+for NUMBER in {33..36}
 do
   echo "NUMBER ${NUMBER}에 대한 테스트를 시작합니다."
   ./tools/dist_test.sh ./projects/configs/bevformer/bevformer_base.py ./ckpts/bevformer_r101_dcn_24ep.pth 8 --prefix test_${NUMBER}
